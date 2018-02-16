a3dd1fe3491db52c21b129392cb2097afabacea0
El Mehdi EL BRINI <elmehdi.elbrini@gmail.com>
2018-02-16 23:40:14 +0100

    Added Logger to spark utils wrappers

--- src/main/scala/fr/s3ni0r/utils/spark/Utils.scala
+++ src/main/scala/fr/s3ni0r/utils/spark/Utils.scala
@@ -3,60 +3,92 @@ package fr.s3ni0r.utils.spark
 import java.time.format.DateTimeFormatter
 import java.time.{Instant, ZoneOffset}
 
+import org.apache.log4j.{Level, LogManager, Logger}
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 trait Utils {
 
-  def getCurrentDate() = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmX")
-    .withZone(ZoneOffset.UTC)
-    .format(Instant.now())
+  def getCurrentDate() =
+    DateTimeFormatter
+      .ofPattern("yyyyMMdd'T'HHmmX")
+      .withZone(ZoneOffset.UTC)
+      .format(Instant.now())
 
-  def withSparkContexts(appName: String = s"Job_${getCurrentDate()}")(block: SparkContext ⇒ Any) = {
-    val conf = new SparkConf().setAppName(s"$appName")
-    val sparkContext = new SparkContext(conf)
-    try {
-      block.apply(sparkContext)
-    } finally {
-      sparkContext.stop()
-    }
-  }
-
-  def withSparkHiveContexts(appName: String = s"Job_${getCurrentDate()}")(block: (SparkContext, HiveContext) ⇒ Any) = {
-    val conf = new SparkConf().setAppName(s"$appName")
-    val sparkContext = new SparkContext(conf)
-    val hiveContext = new HiveContext(sparkContext)
-    try {
-      block.apply(sparkContext, hiveContext)
-    } finally {
-      sparkContext.stop()
-    }
-  }
-
-  def withSparkSqlContexts(appName: String = s"Job_${getCurrentDate()}")(block: (SparkContext, SQLContext) ⇒ Any) = {
-    val conf = new SparkConf().setAppName(s"$appName")
-    val sparkContext = new SparkContext(conf)
-    val sqlContext = new SQLContext(sparkContext)
-    try {
-      block.apply(sparkContext, sqlContext)
-    } finally {
-      sparkContext.stop()
-    }
-  }
-
-  def withSparkSqlHiveContexts(appName: String = s"Job_${getCurrentDate()}", isLocal: Boolean = false)
-                              (block: (SparkContext, SQLContext, HiveContext) ⇒ Any) = {
+  def withSparkContext(appName: String = s"Job_${getCurrentDate()}", isLocal: Boolean = false)(
+      block: (SparkContext, Logger) ⇒ Any) = {
+    val log = LogManager.getRootLogger
+    log.setLevel(Level.WARN)
     val conf = isLocal match {
       case true ⇒ new SparkConf().setAppName(s"$appName").setMaster("local[*]")
-      case _ ⇒ new SparkConf().setAppName(s"$appName")
+      case _    ⇒ new SparkConf().setAppName(s"$appName")
+    }
+    val sparkContext = new SparkContext(conf)
+    try {
+      block.apply(sparkContext, log)
+    } catch {
+      case e: Exception ⇒ log.error(e.getMessage)
+    } finally {
+      sparkContext.stop()
+    }
+  }
+
+  def withSparkHiveContexts(appName: String = s"Job_${getCurrentDate()}", isLocal: Boolean = false)(
+      block: (SparkContext, HiveContext, Logger) ⇒ Any) = {
+    val log = LogManager.getRootLogger
+    log.setLevel(Level.WARN)
+    val conf = isLocal match {
+      case true ⇒ new SparkConf().setAppName(s"$appName").setMaster("local[*]")
+      case _    ⇒ new SparkConf().setAppName(s"$appName")
+    }
+    val sparkContext = new SparkContext(conf)
+    val hiveContext  = new HiveContext(sparkContext)
+    try {
+      block.apply(sparkContext, hiveContext, log)
+    } catch {
+      case e: Exception ⇒ log.error(e.getMessage)
+    } finally {
+      sparkContext.stop()
+    }
+  }
+
+  def withSparkSqlContexts(appName: String = s"Job_${getCurrentDate()}", isLocal: Boolean = false)(
+      block: (SparkContext, SQLContext, Logger) ⇒ Any) = {
+    val log = LogManager.getRootLogger
+    log.setLevel(Level.WARN)
+    val conf = isLocal match {
+      case true ⇒ new SparkConf().setAppName(s"$appName").setMaster("local[*]")
+      case _    ⇒ new SparkConf().setAppName(s"$appName")
+    }
+    val sparkContext = new SparkContext(conf)
+    val sqlContext   = new SQLContext(sparkContext)
+    try {
+      block.apply(sparkContext, sqlContext, log)
+    } catch {
+      case e: Exception ⇒ log.error(e.getMessage)
+    } finally {
+      sparkContext.stop()
+    }
+  }
+
+  def withSparkSqlHiveContexts(appName: String = s"Job_${getCurrentDate()}", isLocal: Boolean = false)(
+      block: (SparkContext, SQLContext, HiveContext, Logger) ⇒ Any) = {
+
+    val log = LogManager.getRootLogger
+    log.setLevel(Level.WARN)
+    val conf = isLocal match {
+      case true ⇒ new SparkConf().setAppName(s"$appName").setMaster("local[*]")
+      case _    ⇒ new SparkConf().setAppName(s"$appName")
     }
 
     val sparkContext = new SparkContext(conf)
-    val sqlContext = new SQLContext(sparkContext)
-    val hiveContext = new HiveContext(sparkContext)
+    val sqlContext   = new SQLContext(sparkContext)
+    val hiveContext  = new HiveContext(sparkContext)
     try {
-      block.apply(sparkContext, sqlContext, hiveContext)
+      block.apply(sparkContext, sqlContext, hiveContext, log)
+    } catch {
+      case e: Exception ⇒ log.error(e.getMessage)
     } finally {
       sparkContext.stop()
     }
